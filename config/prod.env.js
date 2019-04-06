5f15b729ef9c67f3891734168dd51306e07d318d
lhd <791150351@qq.comssh-agent>
2019-04-06 17:41:46 +0800

    delete

--- config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://47.102.146.253:1024"'
-}
--- config/test.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const devEnv = require('./dev.env')
-
-module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  BASE_API: '"http://47.102.146.253:1024"'
-})
